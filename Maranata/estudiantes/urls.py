cde3308ec2c69f8f9ecc6f0d0530e77c2587d54c
jeison26Martinez <112595460+jeison26Martinez@users.noreply.github.com>
2022-11-15 15:21:00 -0500

    Se realizó la interfaz de grado

--- Maranata/estudiantes/urls.py
+++ Maranata/estudiantes/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from estudiantes.views import estudiantes 
+from estudiantes.views import estudiantes,grado
+
 
 urlpatterns = [
     path('',estudiantes,name="estudiantes"),
+    path('grado/',grado,name="grado"),
+    
 ]
