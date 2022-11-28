0d9584e5faf754447d834c8542f843099446c30c
jeison26Martinez <112595460+jeison26Martinez@users.noreply.github.com>
2022-11-28 18:06:24 -0500

    2 Modulos hechos
    Se hizo el modulo de gestión usuario y Grado

--- Maranata/estudiantes/urls.py
+++ Maranata/estudiantes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from estudiantes.views import estudiantes,grado,usuario_listar,usuario_buscar,usuario_modificar
-
+from estudiantes.views import estudiantes,grado,usuario_listar,usuario_buscar,usuario_modificar,estudiante_buscar
+from estudiantes.views import estudiante_modificar,estudiante_listar,grado_buscar,grado_modificar,grado_listar
 
 urlpatterns = [
     path('',estudiantes,name="estudiantes"),
@@ -8,4 +8,10 @@ urlpatterns = [
     path('usuarios/',usuario_listar,name="usuarios-listar"),
     path('usuarios/buscar',usuario_buscar,name="usuarios-buscar"),
     path('usuarios/modificar',usuario_modificar,name="usuarios-modificar"),
+    path('estudiantes/buscar',estudiante_buscar,name="estudiante-buscar"),
+    path('estudiante/modificar',estudiante_modificar,name="estudiante-modificar"),
+    path('estudiante/listar',estudiante_listar,name="estudiante-listar"),
+    path('grado/buscar',grado_buscar,name="grado-buscar"),
+    path('grado/modificar',grado_modificar,name="grado-modificar"),
+    path('grado/listar',grado_listar,name="grado-listar"),
 ]
